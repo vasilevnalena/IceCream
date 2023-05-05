c479297a551bb2866022f692f6fc13a28c8f5a5f
vasilevnalena <vasilevnalena.2012@gmail.com>
2023-05-05 12:43:04 +0400

    Updated offer switch(radio button)

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,19 @@
-let loginButton = document.querySelector(".login__button");
-let modalLogin = document.querySelector(".modal-login");
+let buttonMainMenu = document.querySelector(".catalog__button");
+let modalMainMenu = document.querySelector(".modal-catalog-menu");
 
 let buttonSearch = document.querySelector(".search__button");
 let modalSearch = document.querySelector(".modal-search");
 
-function handleElementEvents(element, modalClass, showModalWindow) {
+let buttonLogin = document.querySelector(".login__button");
+let modalLogin = document.querySelector(".modal-login");
+
+let bodyPage = document.querySelector("body");
+let radioButtonOffer1 = document.querySelector("#radio-offer-1");
+let radioButtonOffer2 = document.querySelector("#radio-offer-2");
+let radioButtonOffer3 = document.querySelector("#radio-offer-3");
+
+
+function modalWindowActions(element, modalClass, showModalWindow) {
   element.addEventListener("click", () => {
     modalClass.classList.add(showModalWindow);
   });
@@ -23,5 +32,25 @@ function handleElementEvents(element, modalClass, showModalWindow) {
   });
 }
 
-handleElementEvents(loginButton, modalLogin, "modal-show");
-handleElementEvents(buttonSearch, modalSearch, "modal-show");
+function offerSwitchRadioButton() {
+  radioButtonOffer1.addEventListener("click", () => {
+    bodyPage.classList.add("background-offer-1");
+    bodyPage.classList.add("picture-offer-1");
+  });
+
+  radioButtonOffer2.addEventListener("click", () => {
+    bodyPage.classList.add("background-offer-2");
+    bodyPage.classList.add("picture-offer-2");
+  });
+
+  radioButtonOffer3.addEventListener("click", () => {
+    bodyPage.classList.add("background-offer-3");
+    bodyPage.classList.add("picture-offer-3");
+  });
+}
+
+modalWindowActions(buttonLogin, modalLogin, "modal-show");
+modalWindowActions(buttonSearch, modalSearch, "modal-show");
+modalWindowActions(buttonMainMenu, modalMainMenu, "modal-show");
+
+offerSwitchRadioButton();

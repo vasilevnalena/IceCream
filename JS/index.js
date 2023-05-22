2148971905c844d48387442d8e5b8a47208525eb
vasilevnalena <vasilevnalena.2012@gmail.com>
2023-05-22 14:41:02 +0400

    Added

--- JS/index.js
+++ JS/index.js
@@ -11,34 +11,38 @@ let bodyPage = document.querySelector("body");
 let radioOffer1 = document.querySelector("#radio-offer-1");
 let radioOffer2 = document.querySelector("#radio-offer-2");
 let radioOffer3 = document.querySelector("#radio-offer-3");
-
 let textOfferElement = document.querySelector(".offer-text");
 
-let feedbackButton = document.querySelector(".feedback-button");
-let closeButton = document.querySelector(".close-icon");
-// let darknessBackground = document.querySelector(".darkness");
+let buttonFeedback = document.querySelector(".feedback-button");
 let modalFeedback = document.querySelector(".modal-feedback");
+let closeButton = document.querySelector(".close-icon");
+let darknessBackground = document.querySelector(".darkness");
 
-function popupWindowActions(element, popupClass, showPopupWindow) {
+function popupWindowActions(element, popupClass) {
   element.addEventListener("click", () => {
-    popupClass.classList.add(showPopupWindow);
+    popupClass.classList.add("show-element");
   });
 
   window.addEventListener("keydown", (evt) => {
     if (evt.key === "Escape") {
       evt.preventDefault();
-      popupClass.classList.remove(showPopupWindow);
+      popupClass.classList.remove("show-element");
     }
   });
 
   window.addEventListener("click", (evt) => {
     if (!element.contains(evt.target) && !popupClass.contains(evt.target)) {
-      popupClass.classList.remove(showPopupWindow);
+      popupClass.classList.remove("show-element");
     }
   });
 }
 
-function offerSwitchRadioButton(radioOffer, backgroundOffer, pictureOffer,offerText) {
+function offerSwitchRadioButton(
+  radioOffer,
+  backgroundOffer,
+  pictureOffer,
+  offerText
+) {
   radioOffer.addEventListener("click", () => {
     bodyPage.classList = "";
     bodyPage.classList.add(backgroundOffer);
@@ -48,22 +52,39 @@ function offerSwitchRadioButton(radioOffer, backgroundOffer, pictureOffer,offerT
   });
 }
 
-// function modalWindowActions(element, modalClass, showModalWindow, darknessBackground) {
-//   element.addEventListener("click", () => {
-//     modalClass.classList.add(showModalWindow);
-//     bodyPage.classList.add(darknessBackground);
-//   });
+function modalWindowActions(callButton, modalClass) {
+  callButton.addEventListener("click", () => {
+    modalClass.classList.add("show-element");
+    darknessBackground.classList.add("show-element");
+  });
 
-//   closeButton.addEventListener("click", () => {
-//     modalClass.classList.remove(showModalWindow);
-//     bodyPage.classList.remove(darknessBackground);
-//   });
-// }
+  closeButton.addEventListener("click", () => {
+    modalClass.classList.remove("show-element");
+    darknessBackground.classList.remove("show-element");
+  });
+}
 
-popupWindowActions(buttonLogin, modalLogin, "show-element");
-popupWindowActions(buttonSearch, modalSearch, "show-element");
-popupWindowActions(buttonMainMenu, modalMainMenu, "show-element");
+modalWindowActions(buttonFeedback, modalFeedback);
 
-offerSwitchRadioButton(radioOffer1, "background-offer-1", "picture-offer-1","Крем-брюле и пломбир<br />с малиновым джемом");
-offerSwitchRadioButton(radioOffer2, "background-offer-2", "picture-offer-2","Шоколадный пломбир<br />и лимонный сорбет");
-offerSwitchRadioButton(radioOffer3, "background-offer-3", "picture-offer-3"," Пломбир с помадкой<br />и клубничный щербет");
+popupWindowActions(buttonLogin, modalLogin);
+popupWindowActions(buttonSearch, modalSearch);
+popupWindowActions(buttonMainMenu, modalMainMenu);
+
+offerSwitchRadioButton(
+  radioOffer1,
+  "background-offer-1",
+  "picture-offer-1",
+  "Крем-брюле и пломбир<br />с малиновым джемом"
+);
+offerSwitchRadioButton(
+  radioOffer2,
+  "background-offer-2",
+  "picture-offer-2",
+  "Шоколадный пломбир<br />и лимонный сорбет"
+);
+offerSwitchRadioButton(
+  radioOffer3,
+  "background-offer-3",
+  "picture-offer-3",
+  " Пломбир с помадкой<br />и клубничный щербет"
+);

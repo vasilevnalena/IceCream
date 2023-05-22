177c164cd60affdb2a3cb1ae4601b4dc6d49dfe0
vasilevnalena <vasilevnalena.2012@gmail.com>
2023-05-22 10:45:22 +0400

    Fixed "hits" elements

--- JS/index.js
+++ JS/index.js
@@ -12,21 +12,26 @@ let radioOffer1 = document.querySelector("#radio-offer-1");
 let radioOffer2 = document.querySelector("#radio-offer-2");
 let radioOffer3 = document.querySelector("#radio-offer-3");
 
-function modalWindowActions(element, modalClass, showModalWindow) {
+let feedbackButton = document.querySelector(".feedback-button");
+let closeButton = document.querySelector(".close-icon");
+// let darknessBackground = document.querySelector(".darkness");
+let modalFeedback = document.querySelector(".modal-feedback");
+
+function popupWindowActions(element, popupClass, showPopupWindow) {
   element.addEventListener("click", () => {
-    modalClass.classList.add(showModalWindow);
+    popupClass.classList.add(showPopupWindow);
   });
 
   window.addEventListener("keydown", (evt) => {
     if (evt.key === "Escape") {
       evt.preventDefault();
-      modalClass.classList.remove(showModalWindow);
+      popupClass.classList.remove(showPopupWindow);
     }
   });
 
   window.addEventListener("click", (evt) => {
-    if (!element.contains(evt.target) && !modalClass.contains(evt.target)) {
-      modalClass.classList.remove(showModalWindow);
+    if (!element.contains(evt.target) && !popupClass.contains(evt.target)) {
+      popupClass.classList.remove(showPopupWindow);
     }
   });
 }
@@ -39,9 +44,23 @@ function offerSwitchRadioButton(radioOffer, backgroundOffer, pictureOffer) {
   });
 }
 
-modalWindowActions(buttonLogin, modalLogin, "modal-show");
-modalWindowActions(buttonSearch, modalSearch, "modal-show");
-modalWindowActions(buttonMainMenu, modalMainMenu, "modal-show");
+// function modalWindowActions(element, modalClass, showModalWindow, darknessBackground) {
+//   element.addEventListener("click", () => {
+//     modalClass.classList.add(showModalWindow);
+//     bodyPage.classList.add(darknessBackground);
+//   });
+
+//   closeButton.addEventListener("click", () => {
+//     modalClass.classList.remove(showModalWindow);
+//     bodyPage.classList.remove(darknessBackground);
+//   });
+// }
+
+// modalWindowActions(feedbackButton, modalFeedback, "modal-show","darkness");
+
+popupWindowActions(buttonLogin, modalLogin, "modal-show");
+popupWindowActions(buttonSearch, modalSearch, "modal-show");
+popupWindowActions(buttonMainMenu, modalMainMenu, "modal-show");
 
 offerSwitchRadioButton(radioOffer1, "background-offer-1", "picture-offer-1");
 offerSwitchRadioButton(radioOffer2, "background-offer-2", "picture-offer-2");
